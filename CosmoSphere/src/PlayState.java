fc64a013348f50f7cc924313eac37be7df750b20
temuulenk <temuulen.k@hotmail.com>
2014-02-12 11:18:07 -0500

    Update PlayState.java
    Toggle FPS
    Pause state added
    Added global timer
    Added getters and setters for global timer

--- CosmoSphere/src/PlayState.java
+++ CosmoSphere/src/PlayState.java
@@ -6,28 +6,56 @@ import org.newdawn.slick.SpriteSheet;
 
 public class PlayState extends BasicGameState {
 	
-
-
+	private boolean toggleFPS, pause;
+	private int globalTimer, timer;
 	
-    public PlayState(int state) {
+	public int getGlobalTimer(){return this.globalTimer}
+	
+	
+	public PlayState(int state) {
     	
-
-    	
-    }
+	}
+    
     public void init(GameContainer appgc, StateBasedGame sbg) throws SlickException {
-
+    
     	
+    		
+
+    		
     }
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException { 
     	gc.setAlwaysRender(true);
-    	System.out.println("Test 1");
+    	
      
         
     }
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
-    
     	Input input = gc.getInput();
     	
+    	timer += delta;
+    	if(timer > 1000){
+    		globalTimer ++;
+    		timer = 0;
+    	}
+    	
+    	
+    	
+    	// Toggling FPS 
+    	if(toggleFPS == false && input.getKeyPressed(Input.KEY_F))
+    		toggleFPS = true;
+    	if(toggleFPS == true && input.getKeyPressed(Input.KEY_F))
+    		toggleFPS = false;
+    	
+    	if(toggleFPS)
+    		gc.setShowFPS(true);
+    	else
+    		gc.setShowFPS(false);
+    	
+    	// Entering pause state 
+    	if(input.getKeyPressed(Input.KEY_ESCAPE)
+    		pause = true;
+    	if(pause)
+    		sbg.enterState(2);
     	
     	
 
